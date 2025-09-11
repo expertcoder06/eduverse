29ecf52f6f4c94d50584860f3808900bdfe3a3b4
Sanjay Sharma <sanjay041024@gmail.com>
2025-09-11 16:24:36 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "eduverse-q7upi",
+  "appId": "1:868718268938:web:b595c38c205982912205f3",
+  "storageBucket": "eduverse-q7upi.firebasestorage.app",
+  "apiKey": "AIzaSyARGpTcna4VRU9VA9KxWgLcYmVp9lBwdcY",
+  "authDomain": "eduverse-q7upi.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "868718268938"
 };
 
 
